869dd7fe05f3d47dc56266c0df6a7a878f9cad4d
MarinaTsl <85139068+MarinaTsl@users.noreply.github.com>
2022-02-16 20:27:16 +0200

    Update Assignment_8.py

--- Assignment_8.py
+++ Assignment_8.py
@@ -1,5 +1,3 @@
-Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import random
 
 def frame_score(tow,bis):
